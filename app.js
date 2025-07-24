af860da880f50c32da752e538a4f02169e3b44da
Saajid <salwajid81@gmail.com>
2025-07-24 19:44:07 -0400

    fixed winerySect and response
    winerysect template finished and reponsivedesign fixed.

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.getElementById('navBar');
-    const triggerSect = document.querySelector('.IntroSection');
+    const triggerSect = document.querySelector('.coverCredit');
     window.addEventListener('scroll', function() {
-        const scrollPos = window.scrollY;
-        const scrollThreshold = triggerSect.offsetTop;
-
-        if (scrollPos > scrollThreshold) {
-            if (!nav.classList.contains('visible')) {
-                nav.classList.add('visible'); // fades IN
-            }
+        const triggerRect = triggerSect.getBoundingClientRect();
+        if (triggerRect.top < 0) {
+            nav.classList.add('visible');
         } else {
-            if (nav.classList.contains('visible')) {
-                nav.classList.remove('visible'); // fades OUT
-            }
+            nav.classList.remove('visible');
         }
     });
 });
